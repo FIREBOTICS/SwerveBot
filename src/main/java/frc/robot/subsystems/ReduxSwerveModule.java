018c4bc603dcc6f559e41bb8307a122f5d278a08
Narnillian <ner.baby@gmail.com>
2025-01-10 02:29:39 -0800

    Actually, all modules use Canandmag via SparkMax data port

--- src/main/java/frc/robot/subsystems/ReduxSwerveModule.java
+++ src/main/java/frc/robot/subsystems/ReduxSwerveModule.java
@@ -9,7 +9,8 @@ import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.AbsoluteEncoder;
+import com.reduxrobotics.sensors.canandmag.Canandmag;
+import com.reduxrobotics.sensors.canandmag.CanandmagSettings;
 import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.epilogue.Logged;
@@ -22,7 +23,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 @Logged
-public class SwerveModule extends SubsystemBase {
+public class ReduxSwerveModule extends SubsystemBase {
     private final SparkMax driveMotor;
     private final SparkMax steerMotor;
 
@@ -37,8 +38,9 @@ public class SwerveModule extends SubsystemBase {
     @NotLogged
     private final SparkMaxConfig steerConfig = new SparkMaxConfig();
 
-    private final AbsoluteEncoder encoder;
-
+    private final Canandmag canandmag;
+    @NotLogged
+    private final CanandmagSettings canandmagSettings = new CanandmagSettings();
     private final Rotation2d offset;
 
     /**
@@ -54,13 +56,11 @@ public class SwerveModule extends SubsystemBase {
      * @param steerMotorInverted Steer NEO is inverted.
      * @param steerOffsetRadians Offset of Canandmag reading from forward.
      */
-    public SwerveModule(int moduleID, boolean driveMotorInverted, boolean steerMotorInverted, double steerOffsetRadians) {
+    public ReduxSwerveModule(int moduleID, boolean driveMotorInverted, boolean steerMotorInverted, double steerOffsetRadians) {
         moduleID *= 10;
         int driveMotorID = moduleID+1;
         int steerMotorID = moduleID+2;
-        // int encoderID = moduleID+3; //unnecessary because encoder is in SparkMAX data port
-
-        offset = new Rotation2d(steerOffsetRadians);
+        int canandmagID = moduleID+3;
         
         driveMotor = new SparkMax(driveMotorID,MotorType.kBrushless);
         steerMotor = new SparkMax(steerMotorID,MotorType.kBrushless);
@@ -87,8 +87,6 @@ public class SwerveModule extends SubsystemBase {
             .pid(DriveConstants.steerkP,
                  DriveConstants.steerkI,
                  DriveConstants.steerkD);
-        // steerConfig.absoluteEncoder
-        //     .zeroOffset(steerOffsetRadians / (2 * Math.PI));
         steerMotor.configure(steerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
 
         driveEncoder = driveMotor.getEncoder();
@@ -97,24 +95,28 @@ public class SwerveModule extends SubsystemBase {
         steerController = steerMotor.getClosedLoopController();
         driveController = driveMotor.getClosedLoopController();
 
-        encoder = steerMotor.getAbsoluteEncoder();
+        canandmag = new Canandmag(canandmagID);
+
+        offset = new Rotation2d(steerOffsetRadians);
+
+        initSteerOffset();
     }
     
     /**
-    * Initializes the steer NEO's encoder to the value of the Canandmag, accounting for the offset.
+    * Initializes the steer motor encoder to the value of the Canandmag, accounting for the offset.
     */
     public void initSteerOffset() {
-       steerEncoder.setPosition(getEncoderAngle().getRadians());
+       steerEncoder.setPosition(getCanandmagAngle().getRadians());
     }
     
     /**
-     * Returns the current angle of the wheel
+     * Returns the current angle of the module between 0 and 2 * PI.
      * 
-     * @return The current angle of the wheel
+     * @return The current angle of the module between 0 and 2 * PI.
      */
-    public Rotation2d getEncoderAngle() {
+    public Rotation2d getCanandmagAngle() {        
         double unsignedAngle =
-            (Units.rotationsToRadians(encoder.getPosition())); // % (2 * Math.PI);
+            (Units.rotationsToRadians(canandmag.getAbsPosition()) - offset.getRadians()) % (2 * Math.PI);
         return new Rotation2d(unsignedAngle);
     }
 
@@ -125,7 +127,7 @@ public class SwerveModule extends SubsystemBase {
      */
     public SwerveModulePosition getPosition() {
     return new SwerveModulePosition(
-        driveEncoder.getPosition(), getEncoderAngle());
+        driveEncoder.getPosition(), getCanandmagAngle());
     }
 
     /**
@@ -163,7 +165,7 @@ public class SwerveModule extends SubsystemBase {
     }
 
     public double getRelativeVelocityMetersPerSecond(double thetaRad) {
-        double rel = getEncoderAngle().getDegrees() % 90.0;
+        double rel = getCanandmagAngle().getDegrees() % 90.0;
         if(rel > 90.0 && rel < 270.0) rel *= -1.0;
         return getCurrentVelocityMetersPerSecond() * (rel / 90.0);
     }
@@ -235,6 +237,8 @@ public class SwerveModule extends SubsystemBase {
     }
 
     public void isTestMode(boolean isTestMode) {
-        //unfortunately i don't believe there is a way to enable party mode while attached to a SparkMax
+        canandmagSettings.setDisableZeroButton(!isTestMode);
+        canandmag.setSettings(canandmagSettings, 0.5, 3);
+        canandmag.setPartyMode(isTestMode ? 10 : 0); // if true, blink at 2Hz, else 0
     }
 }
